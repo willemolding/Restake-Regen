6604c4ad217440810986912ecd56c20d413fa4a0
Willem Olding <willemolding@gmail.com>
2024-05-03 21:49:17 +1000

    building linking openzeppelin and toucan contracts

--- packages/foundry/contracts/FundingPool.sol
+++ packages/foundry/contracts/FundingPool.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@toucanprotocol/contracts/pools/biochar.sol";
 
 /**
  * @title FundingPool
@@ -34,7 +35,7 @@ contract FundingPool {
     }
 
     function retire() external {
-        
+
     }
 
     function currentEpoch() internal view returns (uint256) {
