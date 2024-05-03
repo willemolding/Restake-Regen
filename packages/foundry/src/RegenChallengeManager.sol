c9b4f44bc668c88432d6c8fd814f2b227b2ed8fa
Willem Olding <willemolding@gmail.com>
2024-05-04 09:12:47 +1000

    add skeleton of slashing logic

--- packages/foundry/src/RegenChallengeManager.sol
+++ packages/foundry/src/RegenChallengeManager.sol
@@ -5,16 +5,25 @@ import "forge-std/console.sol";
 
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {RegenServiceManager} from "./RegenServiceManager.sol";
 
 contract RegenChallengeManager is CCIPReceiver {
 
-    address public serviceManager;
+    RegenServiceManager public serviceManager;
 
     constructor(address _router, address _serviceManager) CCIPReceiver(_router) {
-        serviceManager = _serviceManager;
+        serviceManager = RegenServiceManager(_serviceManager);
     }
 
     function _ccipReceive(Client.Any2EVMMessage memory message) internal override {
         console.logBytes(message.data);
+
+        (address operator, uint256 epoch, uint256 amount) = abi.decode(message.data, (address, uint256, uint256));
+
+        // check the pledge registry to see if the challenge is valid and should result in slashing
+
+
+        // if so make a slash
+        serviceManager.freezeOperator(operator);
     }
 }
--- packages/foundry/src/RegenServiceManager.sol
+++ packages/foundry/src/RegenServiceManager.sol
@@ -2,19 +2,43 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@eigenlayer-middleware/src/ServiceManagerBase.sol";
+import {RegenChallengeManager} from "./RegenChallengeManager.sol";
 
 contract RegenServiceManager is ServiceManagerBase {
+    RegenChallengeManager public immutable challengeManager;
+
+    /// @notice when applied to a function, ensures that the function is only callable by the `challengeManger` contract.
+    modifier onlyRegenChallengeManager() {
+        require(
+            msg.sender == address(challengeManager),
+            "Not callable by anything other than the registered challenge manager contract"
+        );
+        _;
+    }
+
     constructor(
         IAVSDirectory _avsDirectory,
         IRegistryCoordinator _registryCoordinator,
-        IStakeRegistry _stakeRegistry
+        IStakeRegistry _stakeRegistry,
+        RegenChallengeManager _challengeManager
     )
         ServiceManagerBase(
             _avsDirectory,
-            IPaymentCoordinator(address(0)), // inc-sq doesn't need to deal with payments
+            IPaymentCoordinator(address(0)), // restake-regen doesn't need to deal with payments
             _registryCoordinator,
             _stakeRegistry
         )
     {
+        challengeManager = _challengeManager;
+    }
+
+
+    /// @notice Called by the ChallengeManager to freeze an operator.
+    /// @dev The Slasher contract is under active development and its interface expected to change.
+    ///      We recommend writing slashing logic without integrating with the Slasher at this point in time.
+    function freezeOperator(
+        address operatorAddr
+    ) external onlyRegenChallengeManager {
+        // slasher.freezeOperator(operatorAddr);
     }
 }
