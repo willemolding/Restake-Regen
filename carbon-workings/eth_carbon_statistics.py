3555145ce5d8f9bd04ca430693f1dc24b7c9ea99
Nic Pittman <nacpittman@gmail.com>
2024-05-03 21:30:52 +1000

    and eth energy usage

--- carbon-workings/eth_carbon_statistics.py
+++ carbon-workings/eth_carbon_statistics.py
@@ -69,8 +69,7 @@ while current < end_date:
     current = end + timedelta(days=1)
 
 
-# %%
-# Create Total Validators Dataframe
+# %% Create Total Validators Dataframe
 validators_df = pd.DataFrame(all_data)
 
 # Group by month and sum the validators count
@@ -80,12 +79,51 @@ validators_df = validators_df.groupby("date")["validators"].sum()
 validators_df = validators_df.reset_index()
 validators_df["date"] = pd.to_datetime(validators_df["date"])
 validators_df["total_validators"] = validators_df["validators"].cumsum()
+validators_df = validators_df.set_index("date")
 
 # Plot
 plt.figure(figsize=(10, 5))
-plt.plot(validators_df["date"], validators_df["total_validators"])
+plt.plot(validators_df.index, validators_df["total_validators"])
 plt.title("Number of Validators Over Time")
 plt.xlabel("Date")
 plt.ylabel("Number of Validators")
 plt.grid(True)
 plt.show()
+
+
+# %% Get API for total energy use on Ethereum
+# URL containing the JSON data
+url = "https://ccaf.io/cbeci/api/eth/pos/charts/total_greenhouse_gas_emissions/monthly"
+
+# Fetch the data
+response = requests.get(url)
+data = response.json()["data"]
+
+# Extract timestamps and emission values from the JSON data
+timestamps = [item["x"] for item in data]
+emissions = [item["y"] for item in data]
+
+# Convert UNIX timestamps to datetime objects for plotting
+dates = [datetime.utcfromtimestamp(ts) for ts in timestamps]
+
+# Create a plot
+plt.figure(figsize=(10, 5))
+plt.plot(dates, emissions, marker="o", linestyle="-", color="b")
+plt.title("Monthly Ethereum Emissions Over Time")
+plt.xlabel("Date")
+plt.ylabel("Monthly Emissions (tons CO2 equivalent)")
+plt.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+# %% MERGE TOGETHER
+
+
+emissions_df = pd.DataFrame(emissions)
+emissions_df["date"] = pd.to_datetime(dates)
+emissions_df = emissions_df.set_index("date")
+
+validators_df["emissions_Tco2"] = emissions_df[0]
+
+df = validators_df
