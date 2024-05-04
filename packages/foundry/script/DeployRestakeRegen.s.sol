311b03def00c60cce560aeb3212c0165ebe5df67
Willem Olding <willemolding@gmail.com>
2024-05-04 10:18:50 +1000

    deploy script building but tests failing

---
+++ packages/foundry/script/DeployRestakeRegen.s.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "forge-std/Script.sol";
+import "forge-std/Test.sol";
+
+import {Deployer_M2} from "@eigenlayer-scripts/deploy/devnet/M2_Deploy_From_Scratch.s.sol";
+
+contract DeployPreconf is Deployer_M2 {
+
+}
--- packages/foundry/script/DeployRestakeRegen.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
-
-// import "eigenlayer-scripts/middleware/DeployOpenEigenLayer.s.sol";
-
-// contract DeployPreconf is DeployOpenEigenLayer {
-// }
